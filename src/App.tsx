8551203d29d742573ba453274c64f5cf3614c878
Nandini Tarafdar <nandinitarafdar481@gmail.com>
2026-04-12 18:31:18 +0530

    tailwind suggestion fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 export default function App() {
   return (<div className="bg-pink-500 text-white text-3xl p-10">
     Tailwind is finally working 💖
+    <p className="text-pink-200 font-bold bg-purple-600">
+      Support ticket system
+    </p>
   </div>);
 }
